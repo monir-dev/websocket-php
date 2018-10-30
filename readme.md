f39bd4137c61acb54399438ea9f92a179e0e364a
Monir-dev <echomonir@gmail.com>
2018-10-30 13:01:19 +0600

    WebSocket with php

---
+++ readme.md
@@ -0,0 +1,8 @@
+Instruction:
+1. Clone the repository
+2. cd into the project
+3. install composer dependency by running the command: composer install
+4. run server using this command: php server.php
+5. open index.html file in two different brower.
+6. start chatting... :XD
+
---
+++ server.php
@@ -0,0 +1,54 @@
+<?php
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+    // Make sure composer dependencies have been installed
+    require __DIR__ . '/vendor/autoload.php';
+
+/**
+ * chat.php
+ * Send any incoming messages to all connected clients (except sender)
+ */
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from != $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+    // Run the server application through the WebSocket protocol on port 8080
+    // $app = new Ratchet\App('localhost', 8080);
+    // $app->route('/chat', new Chat());
+    // $app->route('/echo', new Ratchet\Server\EchoServer, array('*'));
+    // $app->run();
+
+$server = IoServer::factory(
+    new HttpServer(new WsServer(new Chat())),
+    8080
+);
+$server->run();
+//     
