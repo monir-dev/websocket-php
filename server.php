08b5d104a1c10e5cd93aa2542454c2513f3a9da7
Md Monir Hossain <echomonir@gmail.com>
2018-10-30 13:10:22 +0600

    bug fixed

--- server.php
+++ server.php
@@ -5,13 +5,9 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
-    // Make sure composer dependencies have been installed
-    require __DIR__ . '/vendor/autoload.php';
+// Make sure composer dependencies have been installed
+require __DIR__ . '/vendor/autoload.php';
 
-/**
- * chat.php
- * Send any incoming messages to all connected clients (except sender)
- */
 class Chat implements MessageComponentInterface {
     protected $clients;
 
@@ -40,15 +36,9 @@ class Chat implements MessageComponentInterface {
     }
 }
 
-    // Run the server application through the WebSocket protocol on port 8080
-    // $app = new Ratchet\App('localhost', 8080);
-    // $app->route('/chat', new Chat());
-    // $app->route('/echo', new Ratchet\Server\EchoServer, array('*'));
-    // $app->run();
-
+// Start Websocket Server and Listen for port 8080
 $server = IoServer::factory(
     new HttpServer(new WsServer(new Chat())),
     8080
 );
 $server->run();
-//     
